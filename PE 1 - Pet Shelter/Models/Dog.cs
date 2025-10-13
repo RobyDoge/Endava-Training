b6030492e6a31a72d7429a0737cf21e78fc6d461
Robert Popa <robyionut23@gmail.com>
2025-10-13 12:34:20 +0300

    Created Initial Structure

---
+++ PE 1 - Pet Shelter/Models/Dog.cs
@@ -0,0 +1,19 @@
+﻿using PetShelter.Interfaces;
+
+namespace PetShelter.Models;
+
+internal class Dog : Animal, IFeedable
+{
+    public bool IsTrained { get; set; }
+    public void Feed()
+    {
+        Console.WriteLine("I was feed thanks Buddy <3");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Wan Wan");
+    }
+
+    public override decimal DayliyCareCost() => base.DayliyCareCost() + 3.0m;
+}
