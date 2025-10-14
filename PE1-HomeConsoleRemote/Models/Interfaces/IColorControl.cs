7b1ded77ea532b4a8448771922471929b627a9c4
Robert Popa <robyionut23@gmail.com>
2025-10-14 11:31:30 +0300

    Added ColorControl Interface

---
+++ PE1-HomeConsoleRemote/Models/Interfaces/IColorControl.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeConsole.Models.Interfaces;
+
+internal interface IColorControl
+{
+    byte RedValue { get; init; }
+    byte GreenValue { get; init; }
+    byte BlueValue { get; init; } 
+    int Temperature { get; init; }
+    void SetColor(byte red, byte green, byte blue);
+    void SetTemperature(int temperature);
+}
--- PE1-HomeConsoleRemote/Models/SmartDevice.cs
+++ PE1-HomeConsoleRemote/Models/SmartDevice.cs
@@ -4,13 +4,20 @@ using HomeConsole.Models.Interfaces;
 namespace HomeConsole.Models;
 
 internal abstract class SmartDevice: IPowerSwitch, ISelfTest
+
 {
     public int Id { get; init; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
+
     private bool IsOn { get; set; }
 
     public void PowerOn() => IsOn = true;
     public void PowerOff() => IsOn = false;
     public bool GetStatus() => IsOn;
     public virtual string SelfTest() => "SmartDevice";
+
+    public void SetSchedule(TimeOnly start, TimeOnly end)
+    {
+        throw new NotImplementedException();
+    }
 }
