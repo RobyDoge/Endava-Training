b2a6c0120b3976fa4d4e225f099cb60109e71ba7
Robert Popa <robyionut23@gmail.com>
2025-10-14 12:33:53 +0300

    Added DeviceRegistry Interface

---
+++ PE1-HomeConsoleRemote/Services/DeviceRegistry.cs
@@ -0,0 +1,5 @@
+﻿namespace HomeConsole.Services;
+
+internal class DeviceRegistry
+{
+}
---
+++ PE1-HomeConsoleRemote/Services/IDeviceRegistry.cs
@@ -0,0 +1,19 @@
+﻿using HomeConsole.Models;
+
+namespace HomeConsole.Services;
+
+internal interface IDeviceRegistry
+{
+    event Action<SmartDevice> DeviceRegistered;
+    event Action<int> DeviceUnregistered;
+
+    bool Register<T>(T device) where T : SmartDevice;
+    bool Unregister(int deviceId);
+
+    bool TryGetDevice(int deviceId, out SmartDevice? device);
+    T? Get<T>(int deviceId) where T : SmartDevice;
+
+    IEnumerable<SmartDevice> GetAll();
+    IEnumerable<T> GetAll<T>() where T : SmartDevice;
+    IEnumerable<TCapability> FindByCapability<TCapability>() where TCapability : class;
+}
