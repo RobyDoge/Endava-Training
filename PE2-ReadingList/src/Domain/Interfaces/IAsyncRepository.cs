d69ed12d1b47ad8ce874144869fee19f1329d56f
RobyDoge <147256897+RobyDoge@users.noreply.github.com>
2025-10-28 12:58:29 +0200

    Updated Repository to make it async

--- PE2-ReadingList/src/Domain/Interfaces/IAsyncRepository.cs
+++ PE2-ReadingList/src/Domain/Interfaces/IAsyncRepository.cs
@@ -2,8 +2,10 @@
 
 public interface IRepository<TKey, T> where T : class
 {
-    Result<List<T>> FetchAll();
-    Result<IQueryable<T>> Select();
-    Result Add(TKey key, T entity);
-    Result Delete(TKey Key);
+    Task<Result<IReadOnlyList<T>>> ListAsync();
+    Task<Result<T>> GetByIdAsync(TKey key);
+    Task<Result> AddAsync(T entity);
+    Task<Result> BulkAddAsync(IEnumerable<T> entities);
+    Task<Result> DeleteAsync(TKey key);
+    Task<Result> UpdateAsync(TKey key, T updatedEntity);
 }
--- PE2-ReadingList/src/Domain/Records/Error.cs
+++ PE2-ReadingList/src/Domain/Records/Error.cs
@@ -6,6 +6,8 @@ public record Error(string Code, string Message)
     public static Error NullValue = new("Error.NullValue", "Null value detected");
     public static Error Add = new("Error.Add", "The value could not be added");
     public static Error Remove = new("Error.Remove", "The value could not be removed");
+    public static Error Get = new("Error.Get", "The value was not found");
+    public static Error Update = new("Error.Update", "The value could not be updated");
     public static Error FromException(Exception ex) =>
             new("Error.Exception", ex.Message);
 }
