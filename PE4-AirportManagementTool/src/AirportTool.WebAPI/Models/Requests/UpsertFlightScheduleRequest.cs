18be162e634b2a0552cc632da730710d09c8eb13
RobyDoge <147256897+RobyDoge@users.noreply.github.com>
2025-12-24 16:33:39 +0200

    added atributes to upsertFlightScheduleRequest

--- PE4-AirportManagementTool/src/AirportTool.WebAPI/Models/Requests/UpsertFlightScheduleRequest.cs
+++ PE4-AirportManagementTool/src/AirportTool.WebAPI/Models/Requests/UpsertFlightScheduleRequest.cs
@@ -1,11 +1,17 @@
-﻿namespace AirportTool.WebAPI.Models.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirportTool.WebAPI.Models.Requests;
 
 public class UpsertFlightScheduleRequest
 {
+    [Range(1, int.MaxValue)]]
     public int? Id { get; init; }
+    [Range(1, int.MaxValue)]
     public int? FlightId { get; init; }
     public DateTime? ScheduledDepartureUtc { get; init; }
     public DateTime? ScheduledArrivalUtc { get; init; }
+    [MaxLength(10)]
     public string? GateCode { get; init; } = null!;
+    [MaxLength(10)]
     public string? AssignedAircraftTailName { get; init; }
 }
